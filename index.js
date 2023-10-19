daff66efcbdc0f2b0c6e9f9e0ae00ebb96a7cdf8
Rakibul88 <rakibulhasan999000@gmail.com>
2023-10-19 23:21:06 +0600

    dynamically fetching data for each card using id from database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const cors=require('cors')
 const app=express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors())
 app.use(express.json())
 const port=process.env.PORT || 5000;
@@ -32,6 +32,13 @@ async function run() {
     app.get('/allcars', async (req, res) => {
         const cursor = carsCollection.find();
         const result = await cursor.toArray();
+        res.send(result);
+    })
+    app.get('/allcars/:id', async (req, res) => {
+        const id = req.params.id;
+        console.log(id)
+        const query = { _id: new ObjectId(id) }
+        const result = await carsCollection.findOne(query);
         res.send(result);
     })
     app.post('/allcars',async(req,res)=>{
