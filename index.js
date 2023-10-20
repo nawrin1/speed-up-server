aab85f8d09e2f6e78e6f2f8145027e3e5cc76e05
Rakibul88 <rakibulhasan999000@gmail.com>
2023-10-20 15:46:29 +0600

    products are updated when update button is clicked

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors())
 app.use(express.json())
-const port=process.env.PORT || 5000;
+const port=process.env.PORT || 3000;
 
 
 
@@ -83,6 +83,27 @@ async function run() {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) }
         const result = await myCartCollection.findOne(query);
+        res.send(result);
+    })
+    app.put('/allcars/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) }
+        const options = { upsert: true };
+        const updated= req.body;
+
+        const cars = {
+            $set: {
+                name: updated.name,
+                brandName: updated.brandName,
+                price: updated.price,
+                image: updated.image,
+                rating: updated.rating,
+                type: updated.type,
+               description: updated.description
+            }
+        }
+
+        const result = await carsCollection.updateOne(filter, cars, options);
         res.send(result);
     })
 
